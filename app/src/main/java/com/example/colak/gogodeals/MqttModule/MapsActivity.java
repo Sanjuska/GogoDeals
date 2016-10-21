7574c880d6d3281a8a42fb7add82dcf1461c7cdd
Olle Renard <Olle.renard@gmail.com>
2016-10-21 17:38:01 +0200

    Google Maps
    Merging Google Maps branch with Master.

---
+++ app/src/main/java/com/example/colak/gogodeals/MqttModule/MapsActivity.java
@@ -0,0 +1,37 @@
+package com.example.colak.gogodeals.MqttModule;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.example.colak.gogodeals.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Gothenburg and move the camera
+        LatLng gothenburg = new LatLng(57.7089, 11.9746);
+        mMap.addMarker(new MarkerOptions().position(gothenburg).title("Marker in Gothenburg"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(gothenburg));
+    }
+}
