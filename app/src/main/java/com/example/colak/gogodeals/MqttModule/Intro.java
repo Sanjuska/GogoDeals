455424d900a38d79bd7da2ef0d4d7d48282d34e4
sanjuska <colak.sanja@yahoo.com>
2016-11-03 22:15:36 +0100

    Intro-Splash added

---
+++ app/src/main/java/com/example/colak/gogodeals/MqttModule/Intro.java
@@ -0,0 +1,29 @@
+package com.example.colak.gogodeals.MqttModule;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.colak.gogodeals.R;
+
+
+
+    //Sanja
+//This class loads the splash screen and switches over to BluetoothActivity after 2 seconds.
+    public class Intro extends Activity {
+        private final int SPLASH_DISPLAY_DURATION = 2000;
+        @Override
+        public void onCreate(Bundle bundle) {
+            super.onCreate(bundle);
+            setContentView(R.layout.intro);
+
+            new Handler().postDelayed(new Runnable(){
+                @Override
+                public void run() {
+                    Intent mainIntent = new Intent(Intro.this,MainActivity.class);
+                    Intro.this.startActivity(mainIntent);
+                    Intro.this.finish();
+                }
+            }, SPLASH_DISPLAY_DURATION);
+        }}
